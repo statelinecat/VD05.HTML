51453bc07754ddffb1de8572487d2235c6b21016
statelinecat <statelinecat@gmail.com>
2024-08-23 19:17:26 +0300

    del errs

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from flask import Flask, render_template
 
+# Создаем экземпляр приложения Flask
 app = Flask(__name__)
 
+# Определяем маршруты для страниц
 @app.route('/')
-def home():
+def index():
     return render_template('home.html')
 
-@app.route('/about')
-def about():
+@app.route('/blog')
+def blog():
     return render_template('about.html')
 
+# @app.route('/contacts')
+# def contacts():
+#     return render_template('contacts.html')
+
+# Запуск приложения
 if __name__ == '__main__':
     app.run(debug=True)
-
-
